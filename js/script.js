79e666a40df3e4e9da7e9433507bb61da7149821
Evangel <oladipoevangel90@gmail.com>
2021-04-16 19:27:32 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const loginCont = document.querySelector('#loginCont');
+// const indexNav = document.querySelector('#indexNav');
+// const mainBody = document.querySelector('body');
+
+// mainBody.appendChild(indexNav);
+
+loginCont.onclick=()=>{
+    location.href = "../views/main.html";
+}
